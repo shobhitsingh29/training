287a0639de73b5015e8baf3db06e5ee100d1602e
shobhitsingh29 <shob@saxobank.com>
2018-02-13 20:46:34 +0530

    added payment and node traverse

--- nodeTraversalTree/nodeTraversalTree.js
+++ nodeTraversalTree/nodeTraversalTree.js
@@ -1,11 +1,11 @@
 class NodeTraversalTree {
     constructor() {
-        const obj = {
+        this.obj = {
             '1': {
                 '2': {
                     '3': {
                         '4': '',
-                        '10': {'11': ''}
+                        '10': {'11': '  '}
                     }
                 },
                 '7': {
@@ -19,46 +19,142 @@ class NodeTraversalTree {
                 }
             }
         };
-      /*  var arr = Array.prototype.slice.call(obj);
-        arr.map(function (val, key) {
-            console.log(val);
-            console.log("key", key);
-
-        })*/
-        this.div0 = document.getElementsByTagName("pre")[0];
-        this.div1 = document.getElementsByTagName("div")[0];
-        this.div0.innerHTML = JSON.stringify(obj, null, 4);
-        this.nodes = '';
-
+        this.divLocationPrev = document.getElementsByTagName("body")[0];
         this.traverse = this.traverse.bind(this);
-        this.traverse(obj);
+        this.traverseJson = this.traverseJson.bind(this);
+        this.flag;
+        this.createBtnNode = this.createBtnNode.bind(this);
+        this.plusHandler = this.plusHandler.bind(this);
+        this.minusHandler = this.minusHandler.bind(this);
+        this.traverse(this.divLocationPrev, this.obj);
+    }
+
+    createBtnNode(type) {
+        let node = document.createElement("button");
+        if (type == "plus") {
+            node.addEventListener("click", this.plusHandler);
+
+            node.innerText = "+";
+        } else {
+            node.addEventListener("click", this.minusHandler);
+
+            node.innerText = "-";
+
+        }
+        return node;
+
+    }
+
+    plusHandler(e) {
+        this.flag = true;
+        let nodeToAdd = prompt("enter node to add");
+        nodeToAdd = parseInt(nodeToAdd);
+        let obj = {};
+        obj[nodeToAdd] = "";
+
+        if (e.target.parentElement.parentElement.childNodes.length > 2) {
+
+            this.traverse(e.target.parentElement.parentElement, obj);
+        } else {
+            this.traverse(e.target.parentElement.parentElement, obj);
+
+
+        }
+        let targetValue = e.target.parentElement.innerText;
+        targetValue = targetValue.replace("+", "");
+        targetValue = targetValue.replace("-", "");
+        this.traverseJson(this.obj, targetValue, this.flag, nodeToAdd);
+
+    }
+
+    minusHandler(e) {
+        this.flag = false;
+        let targetValue = e.target.parentElement.innerText;
+        targetValue = targetValue.replace("+", "");
+        targetValue = targetValue.replace("-", "");
+        this.traverseJson(this.obj, targetValue, this.flag);
+        e.target.parentElement.parentElement.innerHTML = '';
 
 
     }
 
-    traverse(obj) {
+//error hL
+    traverseJson(obj, target, flag, objToAdd) {
         for (let key in obj) {
+            if (key == target) {
+
+
+                if (flag) {
+                    let objAdded = {};
+                    objAdded[objToAdd] = "";
+
+                    obj[key] =Object.assign({},objAdded);
+
+
+                } else {
+                    obj = null;
+                }
+
+            }
+            if (obj != null) {
+                if (typeof obj[key] == "object") {
+
+                    this.traverseJson(obj[key], target, flag, objToAdd);
+
+                } else {
+
+
+                }
+
+
+            }
+        }
+        this.obj=obj;
+    }
+
+
+    traverse(container, obj) {
+        for (let key in obj) {
+
+            let div = document.createElement("ul");
+            let divLi = document.createElement("li");
+            divLi.classList.add("list-item");
+
+
             if (typeof obj[key] == "object") {
-                let div=document.createElement("div");
-                div.innerHTML=key;
-                this.nodes += key;
-                this.nodes += "=>";
+                divLi.innerHTML = key;
+                let divLiBtnPlus = this.createBtnNode("plus");
+                let divLiBtnMinus = this.createBtnNode("minus");
 
-                this.traverse(obj[key]);
+                divLi.appendChild(divLiBtnPlus);
+                divLi.appendChild(divLiBtnMinus);
+                div.appendChild(divLi);
+
+                this.traverse(div, obj[key]);
+
+            } else {
+                let divLiBtnPlus = this.createBtnNode("plus");
+                let divLiBtnMinus = this.createBtnNode("minus");
+
+                divLi.innerHTML = key;
+                divLi.appendChild(divLiBtnPlus);
+                divLi.appendChild(divLiBtnMinus);
+                div.appendChild(divLi);
+
 
             }
-            else {
-                this.nodes += key;
-                this.nodes += "=>";
 
-
-            }
+            container.appendChild(div);
 
         }
 
-        this.div1.innerHTML = this.nodes;
     }
-
 }
 
 const nodeTraversalTree = new NodeTraversalTree();
+nodeTraversalTree.object=nodeTraversalTree.obj;
+setInterval(function () {
+
+    console.log(nodeTraversalTree.object);
+
+},1000);
